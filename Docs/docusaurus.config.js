cfdec1005f8e0f8f2b26ad890ed1e9ea9169b67d
Matt Fancher <142915944+FancMa01@users.noreply.github.com>
2024-02-23 10:34:31 -0700

    updates

--- Docs/docusaurus.config.js
+++ Docs/docusaurus.config.js
@@ -101,32 +101,11 @@ const config = {
             ],
           },
           {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
             title: "More",
             items: [
               {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/hpcc-systems/Tombolo",
               },
             ],
           },
