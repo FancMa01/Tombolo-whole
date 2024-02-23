ff34cb4a0c87ca2287cefddb96d59c25620f7701
Matt Fancher <142915944+FancMa01@users.noreply.github.com>
2024-02-23 12:45:21 -0700

    update docusaurus.config

--- Docs/docusaurus.config.js
+++ Docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: "https://fancma01.github.io/Tombolo-whole/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/Tombolo-whole/",
+  baseUrl: "/Tombolo-whole",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
