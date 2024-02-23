947e9a75a58fb045f561d7da542978ae8b771ca7
Matt Fancher <142915944+FancMa01@users.noreply.github.com>
2024-02-23 12:54:39 -0700

    update

--- Docs/docusaurus.config.js
+++ Docs/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://fancma01.github.io/Tombolo-whole/",
+  url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/Tombolo-whole",
+  baseUrl: "/Tombolo-whole/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
